83f38f2dc0d1dc5a6d4c2d6ac44d88d83e4c9d55
janeljs <jisunlim818@gmail.com>
2022-04-28 20:05:49 +0900

    socket 가지고 놀기

---
+++ socket/src/SocketTest.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class SocketTest {
+
+	private static final int DEFAULT_PORT = 80;
+	private static final String SAMPLE = "{\"test\": 100}";
+
+	@SuppressWarnings("InfiniteLoopStatement")
+	public static void main(String[] args) throws IOException {
+		try (ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT)) {
+			while (true) {
+				Socket socket = serverSocket.accept();
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+				String line = null;
+				do {
+					line = bufferedReader.readLine();
+					System.out.println(line);
+				} while (line != null && !line.equals(""));
+
+				DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+				byte[] body = SAMPLE.getBytes(StandardCharsets.UTF_8);
+				dataOutputStream.write(body);
+				dataOutputStream.flush();
+				socket.close();
+			}
+		}
+	}
+}
